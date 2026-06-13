07c769a1b1596f48e93e7ddac46f1500cbf8ffbc
Vladislav Ivanov <vladoivanov356@gmail.com>
2026-06-13 23:46:48 +0300

    Added LogEntry model and EF Core configuration with PosgreSQL packages

--- LogPulse.Core/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LogPulse.Core;
-
-public class Class1
-{
-
-}
---
+++ LogPulse.Core/LogEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogPulse.Core;
+
+public class LogEntry
+{
+    public Guid Id { get; set; }
+    public DateTime Timestamp { get; set; }
+    public required string  LogLevel { get; set; }
+    public required string Message { get; set; }
+    public  required string Source { get; set; }
+    public string? Exception { get; set; }
+    public required string MachineName { get; set; }
+}
