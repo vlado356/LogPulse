54aecc23e214438df3bb672bc0bb1a019ff5563c
Vladislav Ivanov <vladoivanov356@gmail.com>
2026-06-16 22:24:40 +0300

    feat: add Blazor WebAssembly dashboard and fetch live logs from API

--- LogPulse.IngestionEngine/Program.cs
+++ LogPulse.IngestionEngine/Program.cs
@@ -8,9 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<LogDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+//builder.Services.AddHostedService<Worker>();
 
 var app = builder.Build();
+app.UseCors();
 
 app.MapPost("/api/logs", async (LogEntry log, LogDbContext db) =>
 {
@@ -20,6 +31,16 @@ app.MapPost("/api/logs", async (LogEntry log, LogDbContext db) =>
     await db.SaveChangesAsync();
 
     return Results.Ok(new { message = "Log accepted successfully!" });
+});
+
+app.MapGet("/api/logs", async (LogDbContext db) =>
+{
+    var latestLogs = await db.Logs
+        .OrderByDescending(l => l.Timestamp)
+        .Take(20)
+        .ToListAsync();
+
+    return Results.Ok(latestLogs);
 });
 
 app.Run();
